f0daaf1f53f5b75e23c6ca380942427e8a61474c
Arnavkheterpal12 <71542068+Arnavkheterpal12@users.noreply.github.com>
2021-05-20 19:53:41 +0530

    Add files via upload

---
+++ AI-Mario-Game-Student/main.js
@@ -0,0 +1,28 @@
+noseX="";
+noseY="";
+GameStart="";
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_jump=loadSound("jump.wav");
+	mario_coin=loadSound("coin.wav");
+	mario_gameover=loadSound("gameover.wav");
+	mario_die=loadSound("mariodie.wav");
+	mario_kick=loadSound("kick.wav");
+    setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	instializeInSetup(mario);
+}
+
+function draw() {
+	game()
+}
+
+
+
+
+
+
